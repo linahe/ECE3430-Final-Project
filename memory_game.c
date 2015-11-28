d3985a38eca42ee58c34fca6aed6fcd0f4385329
linahe <lh3su@virginia.edu>
2015-11-28 14:37:06 -0500

    memory_game.c and memory_game.h files

---
+++ memory_game.c
@@ -0,0 +1,37 @@
+#include "memory_game.h"
+
+
+void startGame()
+{
+
+}
+
+void displayPattern()
+{
+
+}
+
+void receiveUserInput()
+{
+
+}
+
+void checkInput()
+{
+
+}
+
+void winGame()
+{
+
+}
+
+void loseGame()
+{
+
+}
+
+void continueGame()
+{
+
+}
---
+++ memory_game.h
@@ -0,0 +1,36 @@
+#ifndef MEMORY_GAME_H_
+#define MEMORY_GAME_H_
+
+#include <msp430.h>
+#include "led_accel.h"
+
+#define PATTERN_LENGTH 10
+
+typedef enum {
+	StartGame, DisplayPattern, UserInput, CheckInput, WinGame, LoseGame
+} GameState;
+
+typedef struct {
+	Direction pattern[PATTERN_LENGTH];
+	Direction userInput[PATTERN_LENGTH];
+	unsigned int index;
+	GameState currentGameState;
+
+} Game;
+
+void startGame();
+
+void displayPattern();
+
+void receiveUserInput();
+
+void checkInput();
+
+void winGame();
+
+void loseGame();
+
+void continueGame();
+
+
+#endif /* MEMORY_GAME_H_ */
